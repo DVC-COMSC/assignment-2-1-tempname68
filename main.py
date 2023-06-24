27d89d162ded157064e9bbc2aae73b855c2d62b3
tempname68 <136948032+tempname68@users.noreply.github.com>
2023-06-24 21:39:54 +0000

    update removed temp

--- main.py
+++ main.py
@@ -9,9 +9,12 @@ def main():
 
     print("\nThe total number of students:\t\t", m_perc + f_perc)
     print("The number of males and females\t\t", m_perc, "  ", f_perc)
-    temp = m_perc
+    #temp = m_perc
+    #m_perc = (m_perc/(m_perc + f_perc))*100
+    #f_perc = (f_perc/(temp + f_perc))*100
+
     m_perc = (m_perc/(m_perc + f_perc))*100
-    f_perc = (f_perc/(temp + f_perc))*100
+    f_perc = 100-m_perc
     print(f"The percentage of males and females\t {m_perc:.2f} {f_perc:.2f}")
 
 
